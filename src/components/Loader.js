1bf7a0f71a501b83b5adc97ea44082c31efe7efa
Dima <benensondimitrii@gmail.com>
2022-08-16 14:15:19 +0300

    created Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./departments/departments.module.scss";
+
+const Loader = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h4>Loading...</h4>
+    </div>
+  );
+};
+
+export default Loader;
