edeaf20064b0a9d847ff2dbeb724fc4d1e3c14f6
Dima <benensondimitrii@gmail.com>
2022-08-17 15:07:50 +0300

    updated DepartmentItem

--- src/components/departments/DepartmentItem.js
+++ src/components/departments/DepartmentItem.js
@@ -1,23 +1,16 @@
 import React from "react";
-import { Doctor, XIcon } from "../../images";
+import { BoardDepartment } from "./department-board";
 import styles from "./departments.module.scss";
 
 const DepartmentItem = ({ item }) => {
   const { name } = item;
+
   return (
     <div className={styles.department}>
       <h3>{name}</h3>
       <h5>Rules:</h5>
-      {/* TODO: Rules display */}
-      <h5>Crew members:</h5>
-      {/* TODO: Implement crew display - this is just a mock */}
-      <div className={styles.crew}>
-        <div className={styles.member}>
-          <XIcon className={styles.removeCrew} />
-          <Doctor className={styles.crewMember} />
-          <span>Doctor name</span>
-        </div>
-      </div>
+      <h6>total seniority is more than 5 years</h6>
+      <BoardDepartment />
     </div>
   );
 };
