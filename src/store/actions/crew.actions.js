8f32ac3a8b2dd4b80f117fa7123f27f121ac51a3
Dima <benensondimitrii@gmail.com>
2022-08-19 20:09:10 +0300

    updated crew action

--- src/store/actions/crew.actions.js
+++ src/store/actions/crew.actions.js
@@ -1,11 +1,20 @@
-import { CREW_FETCH_DATA } from ".";
+import { CREW_FETCH_DATA, UPDATE_CREW } from ".";
 import { getCrew } from "../../api/crew.api";
 
 export const fetchCrew = () => async (dispatch) => {
   const { crew } = await getCrew();
-
+  
   dispatch({
     type: CREW_FETCH_DATA,
+    payload: {
+      crew,
+    },
+  });
+};
+
+export const updateCrew = (crew) => (dispatch) => {
+  dispatch({
+    type: UPDATE_CREW,
     payload: {
       crew,
     },
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,6 +3,9 @@ export const APPLICATION_INIT = "[App] Init";
 
 // Crew
 export const CREW_FETCH_DATA = "[Crew] Fetch data";
+export const UPDATE_CREW = "UPDATE_CREW";
 
 // Departments
 export const DEPARTMENTS_FETCH_DATA = "[Departments] Fetch data";
+
+
