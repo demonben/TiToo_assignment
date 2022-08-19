232c194c717f121180a814c3ee5a411f2c133e0b
Dima <benensondimitrii@gmail.com>
2022-08-19 16:31:17 +0300

    refactored DepartmentItem comp.

--- src/components/departments/DepartmentItem.js
+++ src/components/departments/DepartmentItem.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { BoardDepartment } from "./department-board";
 import styles from "./departments.module.scss";
+import { useDrop } from "react-dnd";
+import { CREW_MEMBER } from "../../constants/crewMember";
+import { RulesDepartment } from "./RulesDepartment";
 
-const DepartmentItem = ({ item }) => {
-  const { name } = item;
+const DepartmentItem = ({ item, crew }) => {
+  const { name, rules } = item;
+  const [board, setBoard] = useState([]);
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: CREW_MEMBER,
+    drop: (item) => addCrewMemberToBoard(item),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver,
+    }),
+  }));
+
+  const deleteBoardMember = (e) => {
+    const updatedBoard = board.filter((item) => {
+      return item.id !== e.id;
+    });
+    setBoard(() => [...updatedBoard]);
+  };
+
+  const addCrewMemberToBoard = (item) => {
+    if (item) {
+      const boardList = crew.filter((member) => item.id === member.id);
+      setBoard((board) => [...board, boardList[0]]);
+    }
+  };
 
   return (
-    <div className={styles.department}>
+    <div className={styles.department} ref={drop}>
       <h3>{name}</h3>
-      <h5>Rules:</h5>
-      <h6>total seniority is more than 5 years</h6>
-      <BoardDepartment />
+      <RulesDepartment rules={rules} />
+      <BoardDepartment board={board} deleteBoardMember={deleteBoardMember} />
     </div>
   );
 };
 
-export default DepartmentItem;
+DepartmentItem.defaultProps = {
+  crew: [],
+};
+
+export { DepartmentItem };
--- src/components/departments/DepartmentsList.js
+++ src/components/departments/DepartmentsList.js
@@ -1,13 +1,12 @@
 import React from "react";
-import DepartmentItem from "./DepartmentItem";
+import {ItemDepartment} from "./department-item";
 import styles from "./departments.module.scss";
 
 const DepartmentsList = ({ list }) => {
-  
   return (
     <div className={styles.list}>
       {list.map((item) => (
-        <DepartmentItem key={item.id} item={item} />
+        <ItemDepartment key={item.id} item={item} />
       ))}
     </div>
   );
