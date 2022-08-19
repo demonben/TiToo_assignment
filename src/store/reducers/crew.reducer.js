7fc2bf99e89683a1cb21c0fbec060a3d3bc9b20f
Dima <benensondimitrii@gmail.com>
2022-08-19 20:08:02 +0300

    updated crew reducer

--- src/store/reducers/crew.reducer.js
+++ src/store/reducers/crew.reducer.js
@@ -1,8 +1,8 @@
-import { CREW_FETCH_DATA } from "../actions";
+import { CREW_FETCH_DATA, UPDATE_CREW } from "../actions";
 import update from "immutability-helper";
 
 const initialState = {
-  list: null,
+  list: [],
   lastUpdated: null,
 };
 
@@ -16,6 +16,7 @@ const updateCrew = (state, { crew }) =>
 
 const actionsRepo = {
   [CREW_FETCH_DATA]: updateCrew,
+  [UPDATE_CREW]: updateCrew,
 };
 
 export default (state = initialState, { type, payload }) =>
