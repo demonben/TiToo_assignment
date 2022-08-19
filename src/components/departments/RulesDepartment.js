73cc9a099177c82860f059c82635650699f27850
Dima <benensondimitrii@gmail.com>
2022-08-19 15:16:43 +0300

    created RulesDepartment comp.

---
+++ src/components/departments/RulesDepartment.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const RulesDepartment = ({ rules }) => {
+  const { seniority, crew } = rules;
+
+  return (
+    <div>
+      <h5>Rules:</h5>
+      <p>
+        Seniority is more than {seniority.min} and less then {seniority.max}{" "}
+        years.
+      </p>
+      <p>
+        Crew should be {crew.doctors} doctors and {crew.nurses} nurses
+      </p>
+    </div>
+  );
+};
+
+export { RulesDepartment };
